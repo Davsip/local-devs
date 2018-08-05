919b7bc7ab2ef11eacb3481aa7caa924962f9cdb
Jason Warner <jcwarner@gmail.com>
2018-08-05 17:41:30 -0500

    commit, but don't merge

--- client/src/pages/Auth/auth0-variables.js
+++ client/src/pages/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'localdevs.auth0.com',
   clientId: 'aIskvCSCdG5FI9BdJwoStQqSbzwC0J4H',
-  callbackUrl: 'http://localdevs.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
