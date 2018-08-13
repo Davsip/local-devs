4e492dfcfeef9b3469f87372352c0829605ffa84
Jason Warner <jcwarner@gmail.com>
2018-08-13 18:56:24 -0500

    fixed routes with page removals and added method to handle apply on landing page

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import App from './App';
-import Home from './pages/Home/Home';
 import Callback from './pages/Callback/Callback';
 import Auth from './pages/Auth/Auth';
 import history from './history';
-import ProjectsAPI from './pages/projectsAPI';
-import Profile from './pages/Profile/Profile';
 import UserDash from './pages/UserDash/UserDash';
 import AdminDash from './pages/AdminDash/AdminDash';
 import UserProfileForm from './pages/UserProfileForm/UserProfileForm';
@@ -25,13 +22,10 @@ export const makeMainRoutes = () => {
       <Router history={history}>
         <div>
           <Route exact path="/" render={(props) => <App auth={auth} {...props} />} />
-          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
             return <Callback {...props} /> 
           }}/>
-          <Route path="/api/projects" component={ProjectsAPI} />
-          <Route path="/profile" render={(props) => <Profile auth={auth} {...props} />} />
           <Route exact path="/myDashboard" render={(props) => <UserDash auth={auth} {...props} />} />
           <Route exact path="/adminDashboard" render ={(props) => <AdminDash auth={auth} {...props} />} />
           <Route exact path="/userProfileForm" render ={(props) => <UserProfileForm auth={auth} {...props} />} />
